71e6bf133f9a44f3789237403cd579c51add9945
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-02 19:33:12 -0300

    Added tests to the ship function that checks for a hit, a hit twice, a miss, if the sunk has sunk and if it hasn't sunk.

---
+++ src/ships.test.js
@@ -0,0 +1,38 @@
+import ship from "./ships";
+
+let testShip;
+
+beforeEach(() => {
+  return (testShip = ship());
+});
+
+test("Hit a square", () => {
+  testShip.hit(1);
+  expect(testShip.hits).toEqual([true, false, false, false]);
+});
+
+test("Hit a square again", () => {
+  testShip.hit(1);
+  testShip.hit(4);
+  expect(testShip.hits).toEqual([true, false, false, true]);
+});
+
+test("Miss a square", () => {
+  testShip.hit(10);
+  expect(testShip.hits).toEqual([false, false, false, false]);
+});
+
+test("Not sunk ship", () => {
+  testShip.hit(1);
+  testShip.hit(2);
+  testShip.hit(3);
+  expect(testShip.isSunk()).toBe(false);
+});
+
+test("Sunk ship", () => {
+  testShip.hit(1);
+  testShip.hit(2);
+  testShip.hit(3);
+  testShip.hit(4);
+  expect(testShip.isSunk()).toBe(true);
+});
