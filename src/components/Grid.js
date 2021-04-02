51a0a4c16bea53341ef8e00aaa23b1e9dacb2303
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-02 19:30:34 -0300

    Modified the squares to show numbers instead of numbers and letters.

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,10 +2,13 @@ import React from "react";
 import "../styles/Grid.css";
 
 function Grid() {
-  const gridLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-  const gridNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  //const gridLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  //const gridNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
-  return (
+  const gridNumbers = [1, 11, 21, 31, 41, 51, 61, 71, 81, 91];
+  const gridLetters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  /*return (
     <div className="grid">
       {gridNumbers.map((number) => {
         return gridLetters.map((letter) => {
@@ -14,6 +17,20 @@ function Grid() {
               key={`${number}${letter}`}
               id={`${number}${letter}`}
             >{`${number}${letter}`}</div>
+          );
+        });
+      })}
+    </div>
+  );*/
+
+  return (
+    <div className="grid">
+      {gridNumbers.map((number) => {
+        return gridLetters.map((letter) => {
+          return (
+            <div key={`${number + letter}`} id={`${number + letter}`}>{`${
+              number + letter
+            }`}</div>
           );
         });
       })}
