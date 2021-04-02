74ad8ae42f2efe5a711f37c654ea25907bec3f98
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-02 19:32:31 -0300

    Added a ship factory function to create a ship. It returns an object with the ship's length, squares, qhich squares were hit and two functions to update the hits array and to check if the ship has sunk.

---
+++ src/ships.js
@@ -0,0 +1,19 @@
+function ship() {
+  const length = 4;
+  const squares = [1, 2, 3, 4];
+  let hits = [false, false, false, false];
+
+  function hit(number) {
+    if (squares.includes(number)) {
+      hits[squares.indexOf(number)] = true;
+    }
+  }
+
+  function isSunk() {
+    return hits.every((hit) => hit === true);
+  }
+
+  return { length, squares, hits, hit, isSunk };
+}
+
+export default ship;
