6cb4469ba1dbbe08d93bb8439ba2e3aed55cb3b4
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-03 16:58:58 -0300

    Added a function that validates the ship and returns true if the ship is inside the gameboard grid.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,15 +1,34 @@
 //import ship from "./ships";
 
-function gameboard(ships) {
-  const board = new Array(100).fill(false);
+function gameboard() {
+  //const board = new Array(100).fill(false);
+  let board = [];
 
-  ships.forEach((item) => {
+  function isShipValid(ship) {
+    const lastSquare = ship.squares[ship.length - 1];
+
+    if (lastSquare.x > 9 || lastSquare.y > 9) {
+      return false;
+    }
+    return true;
+  }
+
+  /*function generateShip() {
+    const availableFirstSquareHorizontal = [];
+    const availableFirstSquareVertical = [];
+
+    board.forEach(square => {
+      if ()
+    })
+  }*/
+
+  /*ships.forEach((item) => {
     item.squares.forEach((square) => {
       board[square] = true;
     });
-  });
+  });*/
 
-  return { board };
+  return { board, isShipValid };
 }
 
 export default gameboard;
