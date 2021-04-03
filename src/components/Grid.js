b785abe10ea6bb3f46f0d5b1cf24d806f8557779
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-03 14:42:51 -0300

    Changed the first grid number from 1 to 0.

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,9 +3,9 @@ import "../styles/Grid.css";
 
 function Grid() {
   //const gridLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-  //const gridNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  //const gridNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-  const gridNumbers = [1, 11, 21, 31, 41, 51, 61, 71, 81, 91];
+  const gridNumbers = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90];
   const gridLetters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   /*return (
@@ -14,9 +14,9 @@ function Grid() {
         return gridLetters.map((letter) => {
           return (
             <div
-              key={`${number}${letter}`}
-              id={`${number}${letter}`}
-            >{`${number}${letter}`}</div>
+              key={`${letter}${number}`}
+              id={`${letter}${number}`}
+            >{`${letter}${number}`}</div>
           );
         });
       })}
