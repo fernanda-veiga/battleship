b21bcb77b6696217daecf2bfb8c9dd9dbb1ec1f0
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-05 10:05:14 -0300

    Added a function to add a randomly generated ship to the ships array. Returns an object that stores all ships on the gameboard.

--- src/factory/gameboard.js
+++ src/factory/gameboard.js
@@ -7,33 +7,18 @@ function gameboard() {
   function addShip(length) {
     const direction =
       Math.floor(Math.random() * 2) === 0 ? "horizontal" : "vertical";
-    console.log(length);
-    console.log(ships);
-    console.log(direction);
     const validSquares = generateValidSquares(length, ships, direction);
 
     const randomSquare =
       validSquares[Math.floor(Math.random() * validSquares.length)];
-    console.log("random square:");
-    console.log(randomSquare);
 
     ships.push(ship(length, randomSquare, direction));
   }
 
-  addShip(4);
-  addShip(3);
-  addShip(3);
-  addShip(2);
-  addShip(2);
-  addShip(2);
-  addShip(1);
-  addShip(1);
-  addShip(1);
-  addShip(1);
+  const shipsLength = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
+  shipsLength.forEach((length) => addShip(length));
 
-  console.log(ships);
-
-  return { addShip };
+  return { ships };
 }
 
 export default gameboard;
