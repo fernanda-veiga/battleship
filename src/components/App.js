ed283b049c9cbd052eebc5f444583a6c2a7ff71c
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-05 12:06:31 -0300

    Added a newGame constant that represents the current game and stores the game's player. Added a second Grid component and passed the player name and the ships.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Grid from "./Grid";
+import game from "../factory/game";
 import "../styles/App.css";
+
+const newGame = game();
 
 function App() {
   return (
     <div className="App">
-      <Grid />
+      <Grid player="player1" ships={newGame.player1.board} />
+      <Grid player="player2" ships={newGame.player2.board} />
     </div>
   );
 }
