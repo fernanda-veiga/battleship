c56ed7fc38ecb0a9938db55c2f18b098721926a5
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-06 13:21:04 -0300

    Added a grid container that contains the grid, the coordinate labels and a grid description for the player's grid and the opponent's grid. Added an array of id's that contain all the id numbers.

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -4,30 +4,39 @@ import "../styles/Grid.css";
 function Grid(props) {
   const gridX = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const gridY = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const gridCoordTop = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+  const gridCoordLeft = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
-  let idWithShip = [];
-
-  console.log(props.ships);
-
-  props.ships.forEach((ship) => {
-    ship.squares.forEach((square) => {
-      idWithShip.push(`${square.x}${square.y}`);
-    });
-  });
+  let idArray = [];
+  for (let x = 0; x < gridX.length; x++) {
+    for (let y = 0; y < gridY.length; y++) {
+      idArray.push(gridX[x] + gridY[y]);
+    }
+  }
 
   return (
-    <div className="grid">
-      {gridX.map((x) => {
-        return gridY.map((y) => {
-          return idWithShip.includes(x + y) ? (
-            <div key={`${x + y}`} id={`${x + y}`} className="ship">{`${
-              x + y
-            }`}</div>
-          ) : (
-            <div key={`${x + y}`} id={`${x + y}`}>{`${x + y}`}</div>
+    <div className="Grid-container">
+      <div className="Grid-coord-top">
+        {gridCoordTop.map((letter) => {
+          return <div key={`${props.player}-coord-${letter}`}>{letter}</div>;
+        })}
+      </div>
+      <div className="Grid-coord-left">
+        {gridCoordLeft.map((number) => {
+          return <div key={`${props.player}-coord-${number}`}>{number}</div>;
+        })}
+      </div>
+      <div className={`Grid ${props.player}`}>
+        {idArray.map((id) => {
+          return (
+            <div
+              key={`${props.player}-${id}`}
+              id={`${props.player}-${id}`}
+            ></div>
           );
-        });
-      })}
+        })}
+      </div>
+      <div className="Grid-description">{props.description}</div>
     </div>
   );
 }
