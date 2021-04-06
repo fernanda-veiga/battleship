c04ad008ecfa7f856eef43bd8831c457a68a7a8c
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-06 13:52:37 -0300

    Added a button to randomize the player 1 grid and generate new ship positions.

--- src/components/App.js
+++ src/components/App.js
@@ -6,13 +6,17 @@ import "../styles/App.css";
 function App() {
   useEffect(() => {
     const newGame = game();
-    console.log(newGame);
+    const randomizeBtn = document.querySelector(".randomize-btn");
+    randomizeBtn.addEventListener("click", newGame.player1.randomizeBoard);
   }, []);
 
   return (
     <div className="App">
       <Grid player="player1" description="Your grid" />
       <Grid player="player2" description="Opponent's grid" />
+      <button type="button" className="randomize-btn">
+        Randomize grid
+      </button>
     </div>
   );
 }
