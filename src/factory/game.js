875f1a110e76e3771a701abeb9f877226bb4cc9d
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-06 15:21:36 -0300

    Added a function that adds a message to the info displays depending on the game's situation.

--- src/factory/game.js
+++ src/factory/game.js
@@ -14,6 +14,8 @@ function game() {
         return;
       }
 
+      const infoDisplay = document.querySelector(".info-display");
+
       //Player1 plays
       player1.playedSquare = {
         x: Number(div.id.slice(8).split("")[0]),
@@ -24,7 +26,7 @@ function game() {
       player2.getHit(player1.playedSquare);
       if (player2.sunkAllShips()) {
         div.removeEventListener("click", gameRound);
-        console.log("All player 2 ships sunk");
+        infoDisplay.textContent = addInfoToDisplay(player2.name);
         return;
       }
 
@@ -38,14 +40,44 @@ function game() {
       player1.getHit(player2.playedSquare);
       if (player1.sunkAllShips()) {
         div.removeEventListener("click", gameRound);
-        console.log("All player 1 ships sunk");
+        infoDisplay.textContent = addInfoToDisplay(player1.name);
         return;
       }
 
+      infoDisplay.textContent =
+        addInfoToDisplay(player2.name, player1.playedSquare) +
+        " " +
+        addInfoToDisplay(player1.name, player2.playedSquare);
       div.removeEventListener("click", gameRound);
       return;
     });
   });
+
+  function addInfoToDisplay(player, playedSquare = undefined) {
+    if (playedSquare === undefined) {
+      if (player === player2.name)
+        return "All your opponent's ships sunk. You won!";
+      return "All your ships ships sunk. Your opponent won!";
+    }
+
+    const playedDiv = document.querySelector(
+      `#${player}-${playedSquare.x}${playedSquare.y}`
+    );
+    console.log(playedDiv);
+
+    if (player === player2.name) {
+      if (playedDiv.classList.contains("sunk"))
+        return "You sunk one of your opponent's ships.";
+      if (playedDiv.classList.contains("ship"))
+        return "You hit one of your opponent's ships.";
+      return "You missed.";
+    }
+    if (playedDiv.classList.contains("sunk"))
+      return "Your opponent sunk one of your ships.";
+    if (playedDiv.classList.contains("ship"))
+      return "Your opponent hit one of your ships.";
+    return "Your opponent missed.";
+  }
 
   return { player1, player2 };
 }
