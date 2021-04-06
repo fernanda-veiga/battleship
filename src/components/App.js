3acb0cac9677c7267d678f122c5f645dba0cfe5f
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-04-06 13:16:36 -0300

    Added a newGame to the App component. Added a description to the Grid component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Grid from "./Grid";
 import game from "../factory/game";
 import "../styles/App.css";
 
-const newGame = game();
-
 function App() {
+  useEffect(() => {
+    const newGame = game();
+    console.log(newGame);
+  }, []);
+
   return (
     <div className="App">
-      <Grid player="player1" ships={newGame.player1.board} />
-      <Grid player="player2" ships={newGame.player2.board} />
+      <Grid player="player1" description="Your grid" />
+      <Grid player="player2" description="Opponent's grid" />
     </div>
   );
 }
 
 export default App;
+/*ships={newGame.player1.board}*/
